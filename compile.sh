daa946b1c406f1bf14f36f46c4a823b9a4d9f2e5
julio1980p <perezduranjulio@gmail.com>
2019-06-07 17:57:06 -0400

    archivo de compilacion

---
+++ compile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# compilar hadoop 3.1.1 por default
+version=${1:-3.1.1}
+
+cd /hadoop-$version-src
+
+echo -e "\n\ncomile hadoop $version..."
+mvn package -Pdist,native -DskipTests -Dtar
+
+if [[ $? -eq 0 ]]; then
+	echo -e "\n\ncomile hadoop $version compilacion exitosa!\n\n"
+	#mv ./hadoop-dist/target/hadoop-$version.tar.gz /binary
+else
+        echo -e "\n\ncomile hadoop $version compilacion fallida!\n\n"
+fi
